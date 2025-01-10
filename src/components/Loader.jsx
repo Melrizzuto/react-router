a325c714b0203d7a080fbc45b393604dd1bbbfe6
Melrizzuto <melrizzuto4@gmail.com>
2025-01-10 15:41:14 +0100

    added style to loader (spinner fa)

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 export default function Loader() {
     return (
         <div id="loader">
-            <h1>Loading</h1>
+            <span><i className="fa-solid fa-spinner fa-spin-pulse"></i></span>
         </div>
     )
 }
